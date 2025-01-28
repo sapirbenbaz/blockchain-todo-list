898c2f86794049632ea6d6aa06ee26828cd650f6
sapirbenbaz <307938068Sap>
2025-01-29 00:34:02 +0200

    Small change

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { BlockchainService } from 'src/blockchain/blockchain.service';
 
 @Injectable()
 export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+
   constructor(private readonly blockchainService: BlockchainService) {}
 
   create(createTaskDto: CreateTaskDto) {
+    this.logger.log('Adding a new task');
     return this.blockchainService.addTask(createTaskDto.description);
   }
 
   findAll() {
+    this.logger.log('Getting task');
     return this.blockchainService.getTasks();
   }
 
   completeTask(id: BigInt) {
+    this.logger.log('Completing new task');
     return this.blockchainService.completeTask(id);
   }
 }
